27fcb39848331ea730425e1d97200f260c2b1d6a
Ahmed Yehia <98207790+AhmedHosny2@users.noreply.github.com>
2024-06-27 13:50:29 +0300

    connect to DB

--- Server/db_config.js
+++ Server/db_config.js
@@ -1,2 +1,10 @@
-import mysql from "mysql2"
+const mysql = require('mysql2')
 
+const pool = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "bookStore"
+}).promise()
+
+module.exports = pool;
