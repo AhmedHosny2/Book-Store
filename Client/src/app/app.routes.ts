16494eebde770b24b5eda68c8ec412596a425997
Ahmed Yehia <98207790+AhmedHosny2@users.noreply.github.com>
2024-06-28 10:01:54 +0300

    display all authors

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import  {BookComponent} from "./book/book.component"
+import {AuthorsComponent} from "./authors/authors.component"
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'book/:id', component: BookComponent}
+    {path: 'book/:id', component: BookComponent},
+    {path: 'authors', component: AuthorsComponent}
 
 ];
---
+++ Client/src/app/authors/authors.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../services/book/book.service';
+import { NgFor,NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-authors',
+  standalone: true,
+  imports: [NgFor, FormsModule,NgIf],
+
+  templateUrl: './authors.component.html',
+  styleUrl: './authors.component.css'
+})
+export class AuthorsComponent  implements OnInit{
+  authors: any[] = [];
+  newAuthor = {
+    name: '',
+    bio: '',
+    email: ''
+  };
+  currentPage: number = 1;
+  itemsPerPage: number = 3;
+  constructor(private bookService: BookService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.fetchAuthors();
+    console.log(this.authors);
+  }
+
+  fetchAuthors(): void {
+    this.bookService.getUniqueAuthors().subscribe(data => {
+      this.authors = data;
+      console.log(this.authors);
+      
+    });
+  }
+  goToBookDetails(bookId: number) {
+    this.router.navigate(['/book', bookId]);
+  }
+  get paginatedAuthors(): any[] {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    return this.authors.slice(startIndex, startIndex + this.itemsPerPage);
+  }
+
+  // Function to handle changing the current page
+  onPageChange(pageNumber: number) {
+    this.currentPage = pageNumber;
+  }
+  // addAuthor(): void {
+  //   this.authorService.addAuthor(this.newAuthor).subscribe(() => {
+  //     this.getAuthors();
+  //     this.newAuthor = { name: '', bio: '', email: '' };
+  //   });
+  // }
+}
